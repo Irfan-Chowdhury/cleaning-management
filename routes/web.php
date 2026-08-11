5295be97139c102f29d6fe031dd883446fb46b26
Md Irfan Chowdhury <irfanchowdhury80@gmail.com>
2026-08-11 23:00:15 +0600

    some configuration setup

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
     return view('layouts.app');
+});
+
+
+
+Route::get('/clear-cache', function () {
+    Artisan::call('view:clear');
+    Artisan::call('cache:clear');
+    //Artisan::call('optimize:clear');
+    return "Cache cleared successfully!";
 });
